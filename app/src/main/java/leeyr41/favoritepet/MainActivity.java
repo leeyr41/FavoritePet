6297552a009061e7177054116a401739513738d5
leeyr41 <leeyr41@naver.com>
2017-03-30 19:21:46 +0900

    #1 Save Image and String

---
+++ app/src/main/java/leeyr41/favoritepet/MainActivity.java
@@ -0,0 +1,15 @@
+package leeyr41.favoritepet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static leeyr41.favoritepet.R.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+    }
+}
